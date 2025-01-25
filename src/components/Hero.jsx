2fe59a8b454e7012e57eea883e68ea12ceaa039b
benidama <benidama02@gmail.com>
2025-01-25 09:59:06 +0000

    add changes about home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,20 @@
-import profile from "../images/bjd-profile.jpg";
+import profile from "../images/me.jpg";
+import { useState } from "react";
 
 const Hero = ({ title = "Hi, I'm Jean Damascene" }) => {
+  let paragraph =
+    "I'am Jean Damascene BENIMANA, a passionate Front-End Developer with a knack for crafting visually appealing, user-friendly, and responsive web applications. With expertise in HTML, CSS (Tailwind CSS), JavaScript, and React.js, I bring ideas to life through clean, efficient code and intuitive designs. Whether it's building interactive interfaces or solving complex UI challenges, I’m committed to delivering seamless user experiences.";
+
+  const [showMore, setShowMore] = useState(false);
+  if (!showMore) {
+    paragraph = paragraph.substring(0, 120) + "...";
+  }
+
   return (
     <section className="bg-indigo-700 py-10 mb-4 flex flex-row rounded-3xl">
       <div className="mx-5 w-[800px]">
         <img
-          className="w-[255px] h-[160px] rounded-3xl"
+          className="w-[255px] h-[220px] rounded-3xl"
           src={profile}
           alt="My profile"
         />
@@ -17,15 +26,17 @@ const Hero = ({ title = "Hi, I'm Jean Damascene" }) => {
             {title}
           </h1>
 
-          <p className="my-4 md:text-xl text-white text-left text-sm">
-            Hi, I'm Jean Damascene BENIMANA, a passionate Front-End Developer
-            with a knack for crafting visually appealing, user-friendly, and
-            responsive web applications. With expertise in HTML, CSS (Tailwind
-            CSS), JavaScript, and React.js, I bring ideas to life through clean,
-            efficient code and intuitive designs. Whether it's building
-            interactive interfaces or solving complex UI challenges, I’m
-            committed to delivering seamless user experiences.
-          </p>
+          <div>
+            <p className="md:text-xl text-white text-left text-sm">
+              {paragraph}
+            </p>
+            <button
+              onClick={() => setShowMore((prevState) => !prevState)}
+              className="text-cyan-400 mb-5 hover:text-black text-2xl underline"
+            >
+              {showMore ? "Less Info" : "More Info"}
+            </button>
+          </div>
 
           <p className="text-white text-2xl">
             Please, send us your comment by clicking
